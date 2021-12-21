f4ad67873838c612f8a0062768bc8834f426550a
Tissue94 <tissue2979@naver.com>
2021-12-21 22:02:26 +0900

    21.12.21

--- mylist-boot/app/src/main/java/com/eomcs/mylist/ContactController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/ContactController.java
@@ -3,79 +3,88 @@ package com.eomcs.mylist;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController 
 public class ContactController {
 
-  String[] contacts = new String[5];
+  Contact[] contacts = new Contact[5]; // 길이 5인 배열 contacts를 생성
+  int size = 0; //int 변수 size를 선언과 초기화
 
-  int size = 0;
-
-  @RequestMapping("/contact/list")
+  @RequestMapping("/contact/list")  //list 객체 타입, 새로운 배열 arr생성, 초기화.(배열의 크기는 size의 크기와 같다)
   public Object list() {
-    String[] arr = new String[size]; // 배열에 저장된 값만 복사할 새 배열을 만든다
-    for (int i = 0; i < size; i++) {
-      arr[i] = contacts[i]; // 전체 배열에서 값이 들어 있는 항목만 복사한다.
+    Contact[] arr = new Contact[size]; 
+    for (int i = 0; i < size; i++) {  //arr의 배열 크기는 size크기만큼 커진다. 배열 contacts의 값을 배열 arr에 저장한다
+      arr[i] = contacts[i]; 
     }
-    return arr; // 복사한 배열 항목들을 담고 있는 새 배열을 리턴한다.
+    return arr;   //arr배열의 값을 리턴
   }
 
+
   @RequestMapping("/contact/add")
-  public Object add(String name, String email, String tel, String company) {
+  public Object add(Contact contact) {
+    System.out.println(contact); //contact.tostring에서 .tostring은 생략가능
     if (size == contacts.length) { 
-      //기존 배열 대신 새 배열을 연락처 저장 배열로 사용한다.
-      contacts = grow(); // 메서드 이름에서 해당 코드에 대한 설명을 짐작할 수 있다.
+      contacts = grow(); 
     }
-    contacts[size++] = createCSV(name, email, tel, company);
+    contacts[size++] = contact;
     return size;
   }
 
-  @RequestMapping("contact/get")
+  @RequestMapping("/contact/get")
   public Object get(String email) {
     int index = indexOf(email);
     if (index == -1) {
       return "";
-    } 
+    }
+
     return contacts[index];
   }
 
-  @RequestMapping("/contact/update") 
-  public Object update(String name, String email, String tel, String company) {
+  @RequestMapping("/contact/update")
+  public Object update(Contact contact) {
+    int index = indexOf(contact.email);
+    if (index == -1) {
+      return 0;
+    }
+
+    contacts[index] = contact;
+    return 1;
+  }
+
+
+  @RequestMapping("/contact/delete")
+  public Object delete(String email) {
     int index = indexOf(email);
     if (index == -1) {
       return 0;
     }
-    contacts[index] = createCSV(name, email, tel, company);
+
+
+    remove(index);  
     return 1;
+    // 메서드 이름으로 코드의 의미를 짐작할 수 있다. 이것이 메서드로 분리하는 이유이다.
   }
 
-  @RequestMapping("contact/delete")
-  public Object delete(String email) {
-    int index = indexOf(email); 
-    if (index == -1) {
-      return 0;
-    }
-    remove(index);//메서드 이름에서 해당 코드에 대한 의미를 짐작할 수 있다. 이것이 메서드를 분리하는 이유이다
-    return 1;
-  }
+  // 기능: 
+  // - 입력 받은 파라미터 값을 가지고 CSV 형식으로 문자열을 만들어 준다.
+  //
+  // 기능:
+  // - 이메일로 연락처 정보를 찾는다.
+  // - 찾은 연락처의 배열 인덱스를 리턴한다.
+  //
 
-  //기능 : 입력 받은 파라미터 값을 가지고 CSV 형식으로 문자열을 만들어 준다.
-  String createCSV(String name, String email, String tel, String company) {
-    return name + "," + email + "," + tel + "," + company;
-  }
-
-  //기능 : 이메일로 연락처 정보를 찾는다. 찾은 연락처의 배열 인덱스를 리턴한다.
   int indexOf(String email) {
     for (int i = 0; i < size; i++) {
-      if (contacts[i].split(",")[1].equals(email)) { //
+      Contact contact = contacts[i];
+      if (contact.email.equals(email)) { 
         return i;
       }
     }
     return -1;
   }
-  //기능 : 배열에서 지정한 항목을 삭제한다.
 
-  String remove(int index) {
-    String old = contacts[index];
+
+  Contact remove(int index) {
+    Contact old = contacts[index];
     for (int i = index + 1; i < size; i++) {
       contacts[i - 1] = contacts[i];
     }
@@ -83,23 +92,26 @@ public class ContactController {
     return old;
   }
 
-  //기능 : 배열의 크기를 늘린다. 기존 배열의 값을 복사해온다.
-  String[] grow() {
-    // 배열이 꽉 찼다면, 기존배열보다 50% 큰 배열을 새로 만든다.
-    String[] arr = new String[newLength()];
+
+
+  Contact[] grow() {
+    Contact[] arr = new Contact[newLength()];
     copy(contacts, arr);
     return arr;
   }
-
-  // 기능 : 주어진 배열에 대해 50% 증가시킨 새 배열의 길이를 알려준다.
+  // 기능:
+  // - 주어진 배열에 대해 50% 증가시킨 새 배열의 길이를 알려준다.
+  //
   int newLength() {
-    return contacts.length +(contacts.length >> 1);
+    return contacts.length + (contacts.length >> 1);
   }
 
-  // 기능 : 배열을 복사한다.
-  void copy(String[] source, String[] target) {
-    // 개발자가 잘못사용할 것을 대비해서 다음 코드를 추가한다. 
-    // 즉 target 배열이 source 배열보다 작을 경우 target 배열 크기 만큼만 복사한다.
+  // 기능: 
+  // - 배열을 복사한다.
+  // 
+  void copy(Contact[] source, Contact[] target) {
+    // 개발자가 잘못 사용할 것을 대비해서 다음 코드를 추가한다.
+    // 즉 target 배열이 source 배열 보다 작을 경우 target 배열 크기만큼만 복사한다.
     int length = source.length;
     if (target.length < source.length) {
       length = target.length;
@@ -107,6 +119,8 @@ public class ContactController {
     for (int i = 0; i < length; i++) {
       target[i] = source[i];
     }
-  }
-
+  } 
 }
+
+
+
