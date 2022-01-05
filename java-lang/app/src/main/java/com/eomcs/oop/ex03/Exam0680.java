92f6ee9b286f9afe1e4bda5c7c90433946f50742
Tissue94 <tissue2979@naver.com>
2022-01-05 13:57:41 +0900

    22.01.05

--- java-lang/app/src/main/java/com/eomcs/oop/ex03/Exam0680.java
+++ java-lang/app/src/main/java/com/eomcs/oop/ex03/Exam0680.java
@@ -20,33 +20,33 @@ public class Exam0680 {
   }
   // 변수 초기화 문장(variable initializer)을 컴파일 할 때,
   // 1) 스태틱 변수 선언에서 변수 초기화 문장을 별도의 스태틱 블록으로 분리한다.
-  //        static int a;
-  //        static {
-  //          a = 100;
-  //        }
-  //        static {
-  //          a = 200;
-  //          System.out.println("static {} 실행");
-  //        }
-  //        static {
-  //          b = 400;
-  //        }
-  //        static int b;
-  //        static {
-  //          b = 300;
-  //        }
+  //          static int a;
+  //          static {
+  //            a = 100;
+  //          }
+  //          static {
+  //            a = 200;
+  //            System.out.println("static {} 실행");
+  //          }
+  //          static {
+  //            b = 400;
+  //          }
+  //          static int b;
+  //          static {
+  //            b = 300;
+  //          }
   //
   // 2) 스태틱 블록을 한개의 블록으로 합친다. 변수 초기화 문장을 스태틱 블록으로 옮긴다.
-  //        static int a;
-  //        static int b;
-  //        static {
-  //          a = 100;
-  //          a = 200;
-  //          System.out.println("static {} 실행");
-  //          b = 400;
-  //          b = 300;
-  //        }
-  //    
+  //          static int a;
+  //          static int b;
+  //          static {
+  //            a = 100;
+  //            a = 200;
+  //            System.out.println("static {} 실행");
+  //            b = 400;
+  //            b = 300;
+  //          }
+  //      
   // - 바이트 코드(Exam0680$A.class)를 확인해 보라!
 
   public static void main(String[] args) throws Exception {
