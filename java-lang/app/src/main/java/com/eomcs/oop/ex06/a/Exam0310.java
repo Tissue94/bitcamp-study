9d8244c01e9d172e68dc4ce2a417677b644c4fe2
Tissue94 <tissue2979@naver.com>
2022-01-12 14:04:22 +0900

    22.01.12

--- java-lang/app/src/main/java/com/eomcs/oop/ex06/a/Exam0310.java
+++ java-lang/app/src/main/java/com/eomcs/oop/ex06/a/Exam0310.java
@@ -16,6 +16,8 @@ public class Exam0310 {
     // => 형변환(type casting)으로 컴파일러를 속일 수는 있지만,
     //    실행할 때 오류가 발생할 것이다.
     // => 속이지 말라!
+    // Car c = new Sedan(); 이면 가능
+
     Sedan s = (Sedan) c; // 실행할 때 오류 발생! (runtime exception)
     s.sunroof = true;
     s.auto = true;
