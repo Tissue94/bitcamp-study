d60528321e2e81f20ae9867c7d825b1c862384f4
Tissue94 <tissue2979@naver.com>
2022-01-13 10:56:24 +0900

    22.01.13

--- java-lang/app/src/main/java/com/eomcs/oop/ex05/x1/upgrade4/Calculator2.java
+++ java-lang/app/src/main/java/com/eomcs/oop/ex05/x1/upgrade4/Calculator2.java
@@ -1,10 +1,9 @@
-package com.eomcs.oop.ex05.x1.test5;
+package com.eomcs.oop.ex05.x1.upgrade4;
 
 public class Calculator2 {
 
   // plus(), minus()는 기존의 Calculator 클래스에게 위임한다.
-
-  com.eomcs.oop.ex05.x1.test1.Calculator origin = new com.eomcs.oop.ex05.x1.test1.Calculator();
+  com.eomcs.oop.ex05.x1.Calculator origin = new com.eomcs.oop.ex05.x1.Calculator();
 
   public void plus(int value) {
     // 이 클래스가 포함하고 있는 객체에게 실행을 위임한다.
@@ -15,12 +14,22 @@ public class Calculator2 {
     // 이 기능은 기존의 클래스가 처리하도록 기존 객체에게 위임한다.
     origin.minus(value);
   }
+
   // 새 기능 또한 기존 객체의 필드를 사용하여 처리한다.
   public void multiple(int value) {
     origin.result *= value;
   }
 
-  public int getReasult() {
+  public int getResult() {
     return origin.result;
   }
 }
+
+
+
+
+
+
+
+
+
