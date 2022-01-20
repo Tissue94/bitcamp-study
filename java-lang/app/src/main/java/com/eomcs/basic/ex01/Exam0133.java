79631c0df52c8e2d26f21a181dbbd62a73ab606b
Tissue94 <tissue2979@naver.com>
2022-01-20 14:04:23 +0900

    22.01.20

--- java-lang/app/src/main/java/com/eomcs/basic/ex01/Exam0133.java
+++ java-lang/app/src/main/java/com/eomcs/basic/ex01/Exam0133.java
@@ -16,6 +16,27 @@ public class Exam0133 {
 
     // Object로부터 상속 받은 equals() 메서드를 
     // Member 클래스의 역할에 맞게 재정의해보자! => "오버라이딩"
+
+
+    public static void main(String[] args) {
+      Member m1 = new Member("홍길동", 20);
+      Member m2 = new Member("홍길동", 20);
+      Member m3 = new Member("홍길동", 21);
+
+      System.out.println(m1 == m2);
+      System.out.println(m1 == m3);
+      System.out.println(m2 == m3);
+
+      System.out.println(m1.equals(m2));
+      System.out.println(m1.equals(m3));
+      System.out.println(m2.equals(m3));
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(age, name);
+    }
+
     @Override
     public boolean equals(Object obj) {
       if (this == obj)
@@ -27,20 +48,6 @@ public class Exam0133 {
       Member other = (Member) obj;
       return age == other.age && Objects.equals(name, other.name);
     }
-  }
-
-  public static void main(String[] args) {
-    Member m1 = new Member("홍길동", 20);
-    Member m2 = new Member("홍길동", 20);
-    Member m3 = new Member("홍길동", 21);
-
-    System.out.println(m1 == m2);
-    System.out.println(m1 == m3);
-    System.out.println(m2 == m3);
-
-    System.out.println(m1.equals(m2));
-    System.out.println(m1.equals(m3));
-    System.out.println(m2.equals(m3));
   }
 }
 
