552effef3b08f16c1b8e8e6ff5957fbff5ee55e5
Tissue94 <tissue2979@naver.com>
2022-01-26 17:57:33 +0900

    22.01.26

---
+++ java-lang/app/src/main/java/com/eomcs/oop/ex09/a2/after/ToolD.java
@@ -0,0 +1,19 @@
+// 인터페이스 미구현 
+package com.eomcs.oop.ex09.a2.after;
+
+// ToolD 클래스는 Spec 인터페이스에 선언된 메서드를 구현했지만, 
+// 문법적으로
+public class ToolD {
+
+  public void m1() {
+    System.out.println("ToolD.m1()");
+  }
+}
+
+
+
+
+
+
+
+
