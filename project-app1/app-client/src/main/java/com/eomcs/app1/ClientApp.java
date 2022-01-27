f5894553e5c5a61c051c66e068d666ab3caaa48d
Tissue94 <tissue2979@naver.com>
2022-01-27 15:31:33 +0900

    22.01.27

--- project-app1/app-client/src/main/java/com/eomcs/app1/ClientApp.java
+++ project-app1/app-client/src/main/java/com/eomcs/app1/ClientApp.java
@@ -1,6 +1,8 @@
 package com.eomcs.app1;
 
+import java.io.PrintStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ClientApp {
 
@@ -10,6 +12,21 @@ public class ClientApp {
     // 1) 서버 애플맄테이션과 네트워크 연결을 수행한다.
     Socket socket = new Socket("127.0.0.1", 8888); // 서버와 연결될 때까지 객체를 생성하지 않는다.
     System.out.println("서버와 연결되었음!");
+
+    // 데이터를 주고 받기 위한 입출력 스트림을 준비한다.
+    PrintStream out = new PrintStream(socket.getOutputStream());
+    Scanner in = new Scanner(socket.getInputStream());
+
+    // 서버에 데이터를 보낸다.
+    out.println("나");
+
+    // 서버가 응답한 데이터를 읽는다. 
+    String response = in.nextLine(); // 서버가 한 줄의 문자열을 보낼 때까지 리턴하지 않는다.
+    System.out.println("==> " + response);
+
+    // 입출력 도구를 다 사용했으면 도구를 해제한다.
+    out.close();
+    in.close();
 
     // 서버 애플리케이션과 연결 된 것을 끝는다. 
     socket.close();
