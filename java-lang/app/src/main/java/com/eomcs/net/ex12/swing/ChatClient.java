95eab8bc0d3acb7b2547bd4aab6f1f027f40c089
Tissue94 <tissue2979@naver.com>
2022-02-09 17:56:41 +0900

    22.02.09

--- java-lang/app/src/main/java/com/eomcs/net/ex12/swing/ChatClient.java
+++ java-lang/app/src/main/java/com/eomcs/net/ex12/swing/ChatClient.java
@@ -1,60 +1,156 @@
 package com.eomcs.net.ex12.swing;
 
 import java.awt.BorderLayout;
-import java.awt.Button;
+import java.awt.Container;
 import java.awt.FlowLayout;
-import java.awt.Frame;
-import java.awt.Panel;
-import java.awt.TextArea;
-import java.awt.TextField;
+import java.awt.event.ActionEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
 
-public class ChatClient extends Frame {
+public class ChatClient extends JFrame {
   private static final long serialVersionUID = 1L;
 
+  Socket socket;
+  DataInputStream in;
+  DataOutputStream out;
+
+  JTextField addressTf = new JTextField(30);
+  JTextField portTf = new JTextField(5);
+  JTextArea messageListTa = new JTextArea();
+  JTextField messageTf = new JTextField(36);
+
   public ChatClient() {
-    super("계산기");
+    super("채팅!!");
     addWindowListener(new WindowAdapter() {
       @Override
       public void windowClosing(WindowEvent e) {
+        try {in.close();} catch (Exception ex) {}
+        try {out.close();} catch (Exception ex) {}
+        try {socket.close();} catch (Exception ex) {}
         System.exit(0);
       }
     });
-    setSize(400, 300);
+    setSize(500, 400);
 
-    Panel topPanel = new Panel();
+    Container contentPane = this.getContentPane();
+
+
+
+
+    JPanel topPanel = new JPanel();
     topPanel.setLayout(new FlowLayout(FlowLayout.LEFT)); // 기본 레이아웃 관리자를 교체
 
-    TextField addressTf = new TextField(30);
+
     topPanel.add(addressTf);
 
-    TextField portTf = new TextField(4);
+
     topPanel.add(portTf);
 
-    Button connectBtn = new Button("연결");
+    JButton connectBtn = new JButton("연결");
+
+    connectBtn.addActionListener(this::connectChatServer);
+
     topPanel.add(connectBtn);
 
-    add(topPanel, BorderLayout.NORTH);
+    contentPane.add(topPanel, BorderLayout.NORTH);
 
-    TextArea messageListTa = new TextArea();
-    add(messageListTa, BorderLayout.CENTER);
 
-    Panel bottomPanel = new Panel();
+
+    JScrollPane scrollPane = new JScrollPane(messageListTa);
+    contentPane.add(scrollPane, BorderLayout.CENTER);
+
+    JPanel bottomPanel = new JPanel();
     bottomPanel.setLayout(new FlowLayout(FlowLayout.LEFT)); // 기본 레이아웃 관리자를 교체
 
-    TextField messageTf = new TextField(40);
+
     bottomPanel.add(messageTf);
 
-    Button sendBtn = new Button("보내기");
+    JButton sendBtn = new JButton("보내기");
+    sendBtn.addActionListener(this::sendMessage);
     bottomPanel.add(sendBtn);
 
-    add(bottomPanel, BorderLayout.SOUTH);
+    contentPane.add(bottomPanel, BorderLayout.SOUTH);
+
+    messageTf.addActionListener(this::sendMessage);
+
 
     setVisible(true);
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception{
+    UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+    //UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+    //UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+    //System.out.println(UIManager.getSystemLookAndFeelClassName());
+    // UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
     new ChatClient();
   }
+
+  public void connectChatServer(ActionEvent e) {
+    System.out.println("서버에 연결하기!");
+
+    try {
+      socket = new Socket(
+          addressTf.getText(), 
+          Integer.parseInt(portTf.getText()));
+
+      in = new DataInputStream(socket.getInputStream());
+      out = new DataOutputStream(socket.getOutputStream());
+
+      new MessageReceiver(in).start();
+
+    } catch (Exception ex) {
+      // alert창 띄우기
+      JOptionPane.showMessageDialog(this, "서버에 연결 오류!", "통신 오류!", JOptionPane.ERROR_MESSAGE);
+    }
+  }
+
+  public void sendMessage(ActionEvent e) {
+    if (messageTf.getText().length() == 0) {
+      return;
+    }
+
+    try {
+      out.writeUTF(messageTf.getText());
+      out.flush();
+      messageTf.setText("");
+
+    } catch (Exception ex) {
+      JOptionPane.showMessageDialog(this, "메세지 전송 오류!", "통신 오류!", JOptionPane.ERROR_MESSAGE);
+    }
+  }
+
+  class MessageReceiver extends Thread {
+    DataInputStream in;
+
+    public MessageReceiver(DataInputStream in) {
+      this.in = in;
+    }
+
+    @Override
+    public void run() {
+      while (true) {
+        try {
+          String message = in.readUTF();
+          messageListTa.append(message + "\n");
+        } catch (Exception e) {
+
+        }
+      }
+
+    }
+  }
+
 }
+
